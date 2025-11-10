b6b1b522742dc7c33a85f867b13e486169c4588b
taranenko <taranenko@reshenie-soft.ru>
2025-11-10 15:41:19 +0300

    add 121 task solution

---
+++ src/main/java/ru/spb/taranenkoant/leetCode/Task121BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+package ru.spb.taranenkoant.leetCode;
+
+
+/**
+ * {@code @author:} TaranenkoAnt
+ * {@code @createDate:} 10.11.2025
+ */
+public class Task121BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                if (prices[i] - min > maxProfit) {
+                    maxProfit = prices[i] - min;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
