fd5a1253192fc6f1d805e63e0a6c8bc3b8f67fe8
taranenko <taranenko@reshenie-soft.ru>
2025-12-04 18:05:29 +0300

    fix #3

---
+++ src/main/java/ru/spb/taranenkoant/leetCode/Task88MergeSortedArray.java
@@ -0,0 +1,37 @@
+package ru.spb.taranenkoant.leetCode;
+
+
+/**
+ * {@code @author:} TaranenkoAnt
+ * {@code @createDate:} 04.12.2025
+ */
+public class Task88MergeSortedArray {
+
+    public static void main(String[] args) {
+        merge(new int[]{4,5,6,0,0,0}, 3, new int[]{1,2,3}, 3);
+        System.out.println();
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while(i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
